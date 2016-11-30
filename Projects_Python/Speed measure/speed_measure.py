ca10b4487b00c34995959858cdf6f941546e5272
Ivan Kunakhov <kunakhov.ivan@gmail.com>
2016-11-30 11:36:59 +0300

    added test results and graphs

--- Projects_Python/Speed measure/speed_measure.py
+++ Projects_Python/Speed measure/speed_measure.py
@@ -1,4 +1,35 @@
-import matplotlib,pyplot as plt
-plt.plot([1,2,3,4])
-plt.ylabel('sds')
-plt.show()
+#!/usr/bin/python2
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    infoFile = "/home/ivan/git_work/work/Projects_C_C++/cholesky/tests/test.txt"
+    f = open(infoFile, "r")
+    i = 0
+    x_cord = []
+    y_cord = []
+    lst_str = []
+    label= []
+    for line in f:
+        tmp = line.split(" ")
+        length = len(tmp)
+        tmp[length-1] = tmp[length-1][0:3]
+        if i is 0:
+            for point in tmp:
+                x_cord.append(point)
+        else:
+            for point in tmp:
+                lst_str.append(point)
+            y_cord.append(lst_str)
+            lst_str = []
+            label.append("cores "+str(i))
+        i+=1
+
+    k = 0
+    plt.ylabel('TIME', size=14)
+    plt.xlabel('MATRIX SIZE', size=14)
+    while k < (i-1):
+        plt.plot(x_cord, y_cord[k], label=label[k])
+        k+=1
+    plt.legend(loc='upper left')
+    plt.show()
+
